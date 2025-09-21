7fd95f9681d3bc275249afe9302e550b4e00cfd1
perritobigotes3-ops <perritobigotes3@gmail.com>
2025-09-21 07:58:02 -0500

    Update overlay.js

--- public/js/overlay.js
+++ public/js/overlay.js
@@ -1,1 +1,43 @@
-// JS para overlay
+// --- overlay.js ---
+// Conexión con socket.io
+const socket = io();
+const rankingContainer = document.getElementById('ranking');
+const themeStyle = document.getElementById('theme-style');
+
+// Actualizar ranking
+socket.on('state', (state) => {
+  renderRanking(state.participants);
+});
+
+// Cambiar tema dinámicamente
+socket.on('themeChange', (theme) => {
+  if (theme === 'gamer') {
+    themeStyle.href = '/css/style-gamer.css';
+  } else if (theme === 'femenino') {
+    themeStyle.href = '/css/style-femenino.css';
+  }
+});
+
+// Renderizar Top 3
+function renderRanking(participants) {
+  const sorted = Object.entries(participants)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 3);
+
+  rankingContainer.innerHTML = '';
+
+  const medals = ['🥇', '🥈', '🥉'];
+
+  sorted.forEach(([username, coins], index) => {
+    const participant = document.createElement('div');
+    participant.classList.add('participant');
+    participant.innerHTML = `
+      <div class="avatar">
+        <img src="/assets/avatar-placeholder.png" alt="${username}">
+      </div>
+      <div class="name">${username}</div>
+      <div class="medal">${medals[index]}</div>
+    `;
+    rankingContainer.appendChild(participant);
+  });
+}// JS para overlay
